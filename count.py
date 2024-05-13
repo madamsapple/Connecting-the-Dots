bd484af52d7b03c33011327a26241d9b121f2143
Iti Gupta <itig2003@gmail.com>
2024-05-13 18:30:52 +0100

    Cleaned title data

--- count.py
+++ count.py
@@ -1,11 +1,5 @@
-import pandas as pd
-data = pd.read_csv('blah.txt', encoding='unicode_escape')
+f = open("scraped_titles.txt", "r")
+#print(f.read())
 
-df = pd.DataFrame(data, index=None)
-cols = len(df.axes[1])
-print(df)
-print('columns: ' + str(cols))
-count = 0
-# with open('title_collection_final.txt', 'rb') as f:
-#     if (byte := f.read(-1)):
-#         print(byte)
+for x in f:
+    print(x)
