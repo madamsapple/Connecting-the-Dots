87cf61c59b9c2c3009a6863bd69346fa1f11b4ee
Iti Gupta <itig2003@gmail.com>
2024-06-26 15:10:13 +0100

    Update visualisation.js

--- visualisation.js
+++ visualisation.js
@@ -646,7 +646,7 @@ const clock = new THREE.Clock();
 scene = new THREE.Scene();
 
 //adding a camera
-camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 10000);
+camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 100000);
 camera.position.set(0, -1933, 489221);
 
 
